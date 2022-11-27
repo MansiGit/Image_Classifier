8800b51b4f2dfcd89974d17d875c13c62ab7e153
Mansi Borole <mrborole@gmail.com>
2022-11-27 17:22:34 -0500

    trainAndTune completed

--- naiveBayes.py
+++ naiveBayes.py
@@ -12,9 +12,10 @@ class NaiveBayesClassifier(classificationMethod.ClassificationMethod):
   def __init__(self, legalLabels):
     self.legalLabels = legalLabels
     self.type = "naivebayes"
-    self.k = 1 # this is the smoothing parameter, ** use it in your train method **
+    self.k = 0.001 # this is the smoothing parameter, ** use it in your train method **
     self.automaticTuning = False # Look at this flag to decide whether to choose k automatically ** use this in your train method **
-    
+    self.dict_phi_value_per_feature= None
+
   def setSmoothing(self, k):
     """
     This is used by the main method to change the smoothing parameter before training.
@@ -49,9 +50,40 @@ class NaiveBayesClassifier(classificationMethod.ClassificationMethod):
     To get the list of all possible features or labels, use self.features and 
     self.legalLabels.
     """
+    frequency = util.Counter() #initialize a dict that counts the freq of each digit or each frew of T or false for faces
+    for i in trainingLabels: frequency[i]+=1
 
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    for i in frequency.keys():
+      frequency[i]=frequency[i]/len(trainingData) #for face data {0: 0.51483894234 1: 49.324892893643}
+    
+    dict_trainingdata_by_label={key: list() for key in trainingLabels}
+    
+    #segregate training data by label 
+    for i in range(len(trainingData)):
+      dict_trainingdata_by_label[trainingLabels[i]].append(trainingData[i])
+   
+    ###############################
+    # Finding phi values for each pixel for each label
+    ###############################
+    dict_phi_value_per_feature={key: util.Counter() for key in trainingLabels}
+    
+    for label in dict_trainingdata_by_label.keys():
+      temp=util.Counter()
+      cnt=0
+      for d in dict_trainingdata_by_label[label]:
+        cnt+=1
+        for key,value in d:
+          if(value>0):
+            temp[key]+=1  
+      for loc,freq in temp:
+        if(freq==0):
+          freq=self.k
+        temp[loc]=float(freq/cnt)
+      dict_phi_value_per_feature.append(temp)
+
+    self.dict_phi_value_per_feature=dict_phi_value_per_feature
+    #"*** YOUR CODE HERE ***"
+    #util.raiseNotDefined()
         
   def classify(self, testData):
     """
@@ -75,8 +107,8 @@ class NaiveBayesClassifier(classificationMethod.ClassificationMethod):
     """
     logJoint = util.Counter()
     
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    #"*** YOUR CODE HERE ***"
+    #util.raiseNotDefined()
     
     return logJoint
   
