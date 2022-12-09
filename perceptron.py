5de5dffa57d9b4024ad888b349b2145cacb622d4
Hriday Purohit <hridaypurohit@gmail.com>
2022-12-08 20:43:12 -0500

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -17,15 +17,11 @@ class PerceptronClassifier:
         self.features = trainingData[0].keys()
         for iteration in range(self.max_iterations):
             print ("Starting iteration ", iteration, "...")
-            for data_index in range(len(trainingData)):
-              "*** YOUR CODE HERE ***" 
+            for data_index in range(len(trainingData)): 
               y_value = self.classify([trainingData[data_index]])[0]
-
-
               if y_value != trainingLabels[data_index]:
-                #weights vector adjustment
-                  self.weights[trainingLabels[data_index]] += trainingData[data_index]  
-                  self.weights[y_value] -= trainingData[data_index] 
+                  self.weights[trainingLabels[data_index]] = self.weights[trainingLabels[data_index]] + trainingData[data_index]  
+                  self.weights[y_value] = self.weights[y_value] - trainingData[data_index] 
 
            
     def classify(self, data):
@@ -35,13 +31,11 @@ class PerceptronClassifier:
             for l in self.legalLabels:
                 vectors[l] = self.weights[l] * d
             guesses.append(vectors.argMax())
-
         return guesses
 
 
     def findHighWeightFeatures(self, label):
         list_of_weights = []
-
         weight_values = self.weights[label]
 
         for i in range(200):
