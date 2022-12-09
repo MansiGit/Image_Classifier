01177633319851895d96281de915cb8065a30ac6
Hriday Purohit <hridaypurohit@gmail.com>
2022-12-09 17:59:11 -0500

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,5 +1,4 @@
 import util
-
 class PerceptronClassifier:
     def __init__( self, legalLabels, max_iterations):
         self.legalLabels = legalLabels
@@ -23,7 +22,20 @@ class PerceptronClassifier:
                   self.weights[trainingLabels[data_index]] = self.weights[trainingLabels[data_index]] + trainingData[data_index]  
                   self.weights[val_y] = self.weights[val_y] - trainingData[data_index] 
 
-           
+        for iteration in range(self.max_iterations):
+            print("Starting iteration %d..." % iteration)
+            for i in range(len(trainingData)):
+                vectors = util.Counter()
+                for label in self.legalLabels:
+                    vectors[label] = self.weights[label] * trainingData[i]
+
+                #trainingLabels[i] is the true label
+                best_guess_label = vectors.argMax()
+                if trainingLabels[i] != best_guess_label:
+                    self.weights[trainingLabels[i]] += trainingData[i]
+                    self.weights[best_guess_label] -= trainingData[i]
+
+
     def classify(self, data):
         guesses = []
         for dat in data:
